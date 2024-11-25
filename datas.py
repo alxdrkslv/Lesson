863c0de5b83802e159fa77d01348c7a4c0ad2839
alxdrkslv <alexandrrkiselev@gmail.com>
2024-11-25 20:38:02 +0300

    1 homework

---
+++ datas.py
@@ -0,0 +1,5 @@
+print("Hello, world!")         # // - целочисленное деление, / - деление
+print(type('Hello, world!'))   # string - строчка (str)
+print(5 != 5 and 5 > 10)       #boolean
+
+print(type(bool('5')))
